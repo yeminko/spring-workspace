b937983998dc403ea14c18086d8bef1c9cb6c180
Ye Min Ko <yeminko47@gmail.com>
2022-03-10 11:57:11 +0630

    [added] REST API with JWT project

---
+++ rest-with-jwt/src/test/java/com/example/restwithjwt/RestWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restwithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
