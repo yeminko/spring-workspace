71a96f764e72ffad6cb3ec0c90fa0c7acdae2575
Ye Min Ko <yeminko47@gmail.com>
2023-05-17 16:36:09 +0630

    [added] Reading List Project

---
+++ readinglist/src/test/java/com/plandoer/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plandoer.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
