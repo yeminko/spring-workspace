d6bbf2b04d6cb12549dae283c8638bc38268404e
yeminko <yeminko47@gmail.com>
2026-08-10 16:05:15 +0630

    added: implement Parrot class with Spring component annotation; create ProjectConfig for bean definitions; add .gitignore and initial pom.xml

--- 01-01-simple-spring-app/src/main/java/com/plandoer/Main.java
+++ 01-01-simple-spring-app/src/main/java/com/plandoer/Main.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+//        Parrot p = context.getBean("parrot2", Parrot.class);
         Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
     }
