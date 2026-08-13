9133f8708b2ec4ad2c34e82f1eb927f7490fe3ea
yeminko <yeminko47@gmail.com>
2026-08-13 19:11:01 +0630

    added: initialize Parrot name with @PostConstruct annotation; update pom.xml to include jakarta.annotation-api dependency

--- 02-01-stereotypes/src/main/java/com/plandoer/beans/Parrot.java
+++ 02-01-stereotypes/src/main/java/com/plandoer/beans/Parrot.java
@@ -1,10 +1,16 @@
 package com.plandoer.beans;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Parrot {
     private String name;
+
+    @PostConstruct
+    public void init() {
+        name = "Polly";
+    }
 
     public String getName() {
         return name;
